361a0d16b2e43e9a935d77a17fb059917f31b183
Zack Renwick <zrenwick1997@gmail.com>
2023-01-15 22:30:08 +0000

    chore: file cleanup

--- main.ts
+++ main.ts
@@ -2,9 +2,6 @@ import { listenAndServe } from "https://deno.land/std@0.110.0/http/server.ts";
 import { validPaths } from "./validPaths.ts";
 
 const addr = ":8081";
-const ERROR_HTML = await Deno.readFile("./error.html");
-
-const games = JSON.parse(await Deno.readTextFile("./games.json"));
 
 async function handleRequest(request: Request): Promise<Response> {
     const { pathname } = new URL(request.url);
@@ -12,7 +9,8 @@ async function handleRequest(request: Request): Promise<Response> {
     const validPath = validatePathAndReturnPathToServe(pathname);
 
     if ("" === validPath.urlPath) {
-      return new Response(ERROR_HTML, {
+      const errorhtml = await Deno.readFile("./error.html");
+      return new Response(errorhtml, {
         headers: {
           "content-type": "text/html",
         },
@@ -21,6 +19,7 @@ async function handleRequest(request: Request): Promise<Response> {
      
     if ("json" === validPath.extention) {
       const contentType = validPath.contentType + validPath.extention;
+      const games = JSON.parse(await Deno.readTextFile("./data/games.json"));
       if("getRandomGame" === validPath.urlPath) {
         const randomNumber = Math.floor(Math.random() * games.length + 1);
         const randomGame = games[randomNumber];
@@ -32,12 +31,12 @@ async function handleRequest(request: Request): Promise<Response> {
       }
     }
 
-    let fileToRead = "./" + validPath.urlPath + "." + validPath.extention;
+    let fileToRead = "./" + validPath.resourcePath + validPath.urlPath + "." + validPath.extention;
 
     if ("manifest+json" === validPath.extention) {
       fileToRead = "./assets/favicon/" + validPath.urlPath;
     } else if ("png" === validPath.extention) {
-      let path = "Scouts_Logo_Marque_Purple" === validPath.urlPath ? "images/" : "favicon/";
+      const path = "Scouts_Logo_Marque_Purple" === validPath.urlPath ? "images/" : "favicon/";
       fileToRead = "./assets/" + path +  validPath.urlPath + "." + validPath.extention
     }
 
@@ -49,8 +48,8 @@ async function handleRequest(request: Request): Promise<Response> {
     });
 }
 
-function validatePathAndReturnPathToServe(pathname: string): { urlPath: string, contentType: string, extention: string }  {
-  let pathObject: { urlPath: string, contentType: string, extention: string }  = {"urlPath": "", "contentType": "", "extention": ""};
+function validatePathAndReturnPathToServe(pathname: string): { urlPath: string, resourcePath: string, contentType: string, extention: string }  {
+  let pathObject: { urlPath: string, resourcePath: string, contentType: string, extention: string }  = {"urlPath": "", resourcePath: "", "contentType": "", "extention": ""};
   validPaths.forEach(validPath => {
     if (pathname.includes(validPath.urlPath)) {
       pathObject = validPath;
--- validPaths.ts
+++ validPaths.ts
@@ -1,15 +1,15 @@
 export const validPaths = [
-    {"urlPath":"getRandomGame", "contentType": "application", "extention": "json"},
-    {"urlPath":"getAllGames", "contentType": "application", "extention": "json"},
-    {"urlPath":"main", "contentType": "text", "extention": "css"},
-    {"urlPath":"favicon-16x16", "contentType": "image", "extention": "png"},
-    {"urlPath":"favicon-32x32", "contentType": "image", "extention": "png"},
-    {"urlPath":"android-chrome-192x192", "contentType": "image", "extention": "png"},
-    {"urlPath":"android-chrome-512x512", "contentType": "image", "extention": "png"},
-    {"urlPath":"apple-touch-icon", "contentType": "image", "extention": "png"},
-    {"urlPath":"site.webmanifest", "contentType": "application", "extention": "manifest+json"},
-    {"urlPath":"Scouts_Logo_Marque_Purple", "contentType": "image", "extention": "png"},
-    {"urlPath":"randomGame", "contentType": "text", "extention": "html"},
-    {"urlPath":"listOfGames", "contentType": "text", "extention": "html"},
-    {"urlPath":"homepage", "contentType": "text", "extention": "html"}
+    {"urlPath":"getRandomGame", "resourcePath": "","contentType": "application", "extention": "json"},
+    {"urlPath":"getAllGames", "resourcePath": "","contentType": "application", "extention": "json"},
+    {"urlPath":"main", "resourcePath": "","contentType": "text", "extention": "css"},
+    {"urlPath":"favicon-16x16", "resourcePath": "","contentType": "image", "extention": "png"},
+    {"urlPath":"favicon-32x32", "resourcePath": "","contentType": "image", "extention": "png"},
+    {"urlPath":"android-chrome-192x192", "resourcePath": "","contentType": "image", "extention": "png"},
+    {"urlPath":"android-chrome-512x512", "resourcePath": "","contentType": "image", "extention": "png"},
+    {"urlPath":"apple-touch-icon", "resourcePath": "","contentType": "image", "extention": "png"},
+    {"urlPath":"site.webmanifest", "resourcePath": "","contentType": "application", "extention": "manifest+json"},
+    {"urlPath":"Scouts_Logo_Marque_Purple", "resourcePath": "", "contentType": "image", "extention": "png"},
+    {"urlPath":"randomGame", "resourcePath": "pages/", "contentType": "text", "extention": "html"},
+    {"urlPath":"listOfGames","resourcePath": "pages/", "contentType": "text", "extention": "html"},
+    {"urlPath":"homepage", "resourcePath": "pages/", "contentType": "text", "extention": "html"}
 ]
